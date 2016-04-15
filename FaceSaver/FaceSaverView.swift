99cd7e57663f533602a9108b98c63c808fad108f
quinnjn <quinn@neumiiller.com>
2016-04-15 15:03:48 -0700

    Hello Classic DVD player screensaver

--- FaceSaver/FaceSaverView.swift
+++ FaceSaver/FaceSaverView.swift
@@ -12,6 +12,9 @@ import ScreenSaver
 class ImageStreamView: ScreenSaverView {
     
     var image: NSImage?
+    var origin: NSPoint?
+    var dest: NSPoint = NSMakePoint(-1, -2)
+    var bgColor: CGColor = NSColor.blackColor().CGColor
     
     override init?(frame: NSRect, isPreview: Bool) {
         super.init(frame: frame, isPreview: isPreview)
@@ -33,21 +36,43 @@ class ImageStreamView: ScreenSaverView {
     
     override func drawRect(rect: NSRect) {
         let context: CGContextRef = NSGraphicsContext.currentContext()!.CGContext
-        CGContextSetFillColorWithColor(context, NSColor.blackColor().CGColor);
+        CGContextSetFillColorWithColor(context, bgColor);
         CGContextSetAlpha(context, 1);
         CGContextFillRect(context, rect);
         
-        
-        if let image = image {
-            let point = SSRandomPointForSizeWithinRect(image.size, frame);
-            image.drawAtPoint(point, fromRect: NSZeroRect, operation: .CompositeSourceOver, fraction: 1)
-        }
+//        if let image = image {
+//            if let origin = origin {
+//                image.drawAtPoint(origin, fromRect: NSZeroRect, operation: .CompositeSourceOver, fraction: 1)
+//            }
+//        }
     }
     
     override func animateOneFrame() {
+
         if let image = image {
-            let origin = SSRandomPointForSizeWithinRect(image.size, frame)
-            image.drawAtPoint(origin, fromRect: NSZeroRect, operation: .CompositeSourceOver, fraction: 1)
+            if origin == nil {
+                origin = SSRandomPointForSizeWithinRect(image.size, frame)
+            }
+            if let oldOrigin = origin {
+                dest.x = getNewStepDirection(dest.x,
+                    imageSize: image.size.width,
+                    originPoint: oldOrigin.x,
+                    maxPoint: frame.size.width
+                )
+                dest.y = getNewStepDirection(dest.y,
+                    imageSize: image.size.height,
+                    originPoint: oldOrigin.y,
+                    maxPoint: frame.size.height
+                )
+                let newX = oldOrigin.x + dest.x
+                let newY = oldOrigin.y + dest.y
+                
+                origin = NSMakePoint(newX, newY)
+                
+                drawOverOldImage(oldOrigin, imageSize: image.size)
+                image.drawAtPoint(origin!, fromRect: NSZeroRect, operation: .CompositeSourceOver, fraction: 1)
+            }
+
         }
     }
     
@@ -57,6 +82,24 @@ class ImageStreamView: ScreenSaverView {
     
     override func configureSheet() -> NSWindow? {
         return nil
+    }
+    
+    func drawOverOldImage(origin: NSPoint, imageSize: NSSize) {
+        let absDest:NSPoint = NSPoint(x:abs(dest.x), y:abs(dest.y));
+        NSBezierPath.fillRect(NSMakeRect(
+            origin.x-absDest.x,
+            origin.y-absDest.y,
+            imageSize.width+(absDest.x*2),
+            imageSize.height+(absDest.y*2))
+        )
+    }
+    
+    func getNewStepDirection(currentStep:CGFloat, imageSize:CGFloat, originPoint:CGFloat, maxPoint:CGFloat) -> CGFloat {
+        let newPoint = originPoint + currentStep
+        if(newPoint <= 0 || newPoint >= (maxPoint-imageSize)) {
+            return currentStep * -1
+        }
+        return currentStep
     }
     
     func loadImage() {
